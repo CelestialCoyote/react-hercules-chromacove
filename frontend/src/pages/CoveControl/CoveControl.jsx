7c34f02456a3318c3fc6b22810ee97fc32041bbf
Paul Stearns <celestialcoyote@gmail.com>
2023-10-16 20:28:05 -0700

    trying to clean up fetch code for channel data

--- frontend/src/pages/CoveControl/CoveControl.jsx
+++ frontend/src/pages/CoveControl/CoveControl.jsx
@@ -6,131 +6,127 @@ import VCoveSlider from '../../components/VCoveSlider/VCoveSlider';
 import CoveMasterSlider from '../../components/CoveMasterSlider/CoveMasterSlider';
 import { baseAPI } from '../../userConfig/baseAPI';
 import channels from '../../userConfig/channels.json';
+import covePresets from '../../userConfig/presets.json';
 
 
 const CoveControl = () => {
 
-    const [channelData, setChannelData] = useState(channels);
-    const [presets, setPresets] = useState([]);
-    const [isEditOpen, setIsEditOpen] = useState(false);
-    const [masterValue, setMasterValue] = useState(1.000);
-    const [duration, setDuration] = useState(3);
+	const [channelData, setChannelData] = useState([]);
+	const [presets, setPresets] = useState(covePresets);
+	const [isEditOpen, setIsEditOpen] = useState(false);
+	const [masterValue, setMasterValue] = useState(1.000);
+	const [duration, setDuration] = useState(3);
 
-    // State varible for all channels.
-    const [state, setState] = useState({}); // Could probably merge with channelData
-    const DEFAULT_VAL = 0.000;
+	// State varible for all channels.
+	const [state, setState] = useState({}); // Could probably merge with channelData
+	const DEFAULT_VAL = 0.000;
 
-	console.log(channels);
+	const addChannels = (channelData) => {
+		// Mapped channelId to its useState.
+		channelData.forEach(channel => {
+			setState(prevState => ({ ...prevState, [channel.id]: { ...channel, value: DEFAULT_VAL } }));
+		});
+	};
 
-    const addChannels = (channelData) => {
-        // Mapped channelId to its useState.
-        channelData.forEach(channel => {
-            setState(prevState => ({ ...prevState, [channel.id]: { ...channel, value: DEFAULT_VAL } }));
-        });
-    };
+	//Get ChannelData and Preset information from backend.
+	useEffect(() => {
+		try {
+			setChannelData(channels);
+			addChannels(channels);
+		} catch (error) {
+			console.log('Get DMX channel data information failed.', error);
+		}
 
-    //Get ChannelData and Preset information from backend.
-    useEffect(() => {
-        try {
-            // baseAPI.get('channelData')
-            //     .then((res) => {
-                    setChannelData(channels); // Unneeded since the next function mutates the same state
-                    addChannels(channels);
-                //});
-        } catch (error) {
-            console.log('Get DMX channel data information failed.', error);
-        }
+	}, []); // eslint-disable-line react-hooks/exhaustive-deps
+	//Try to find a better solution for the above comment line to remove warning
+	//about missing dependency in useEffect.
 
-    }, []); // eslint-disable-line react-hooks/exhaustive-deps
-    //Try to find a better solution for the above comment line to remove warning
-    //about missing dependency in useEffect.
+	useEffect(() => {
+		try {
+			baseAPI.get('presets')
+				.then((res) => {
+					setPresets(res.data);
+				});
+		} catch (error) {
+			console.log('Get Preset button information failed.', error);
+		}
+	}, []);
 
-    useEffect(() => {
-        try {
-            baseAPI.get('presets')
-                .then((res) => {
-                    setPresets(res.data);
-                });
-        } catch (error) {
-            console.log('Get Preset button information failed.', error);
-        }
-    }, []);
+	if (presets.length === 0) {
+		return <h1>Loading.....</h1>;
+	}
 
-    if (presets.length === 0) {
-        return <h1>Loading.....</h1>;
-    }
+	return (
 
-    return (
+		<>
 
-        <>
+			{!isEditOpen &&
 
-            {!isEditOpen &&
+				<div className="flex flex-1">
 
-                <div className="flex flex-1">
+					<div className="flex flex-1 flex-col w-1/2 m-6">
 
-                    <div className="flex flex-1 flex-col w-1/2 m-6">
+						<div className="flex flex-1 justify-between">
 
-                        <div className="flex flex-1 justify-between">
+							{channelData.map(channel =>
+								<VCoveSlider
+									key={channel.id}
+									channelState={state[channel.id]}
+									setState={setState}
+									masterValue={masterValue}
+									duration={duration}
+								/>
+							)}
 
-                            {channelData.map(channel =>
-                                <VCoveSlider
-                                    key={channel.id}
-                                    channelState={state[channel.id]}
-                                    setState={setState}
-                                    masterValue={masterValue}
-                                    duration={duration}
-                                />
-                            )}
+						</div>
 
-                        </div>
+						<div className="flex flex-col mt-4">
 
-                        <div className="flex flex-col mt-4">
+							<CoveMasterSlider
+								channelData={channelData}
+								channelState={state}
+								setMasterValue={setMasterValue}
+								masterValue={masterValue}
+								duration={duration}
+							/>
 
-                            <CoveMasterSlider
-                                channelData={channelData}
-                                channelState={state}
-                                setMasterValue={setMasterValue}
-                                masterValue={masterValue}
-                                duration={duration}
-                            />
+						</div>
 
-                        </div>
+					</div>
 
-                    </div>
+					<div className="flex flex-col w-1/2">
 
-                    <div className="flex flex-col w-1/2">
+						<CovePresetButtons
+							presets={presets}
+							channelState={state}
+							setState={setState}
+							setMasterValue={setMasterValue}
+							setIsEditOpen={setIsEditOpen}
+						/>
 
-                        <CovePresetButtons
-                            presets={presets}
-                            channelState={state}
-                            setState={setState}
-                            setMasterValue={setMasterValue}
-                            setIsEditOpen={setIsEditOpen}
-                        />
+						<FadeRateSelect
+							duration={duration}
+							setDuration={setDuration}
+						/>
 
-                        <FadeRateSelect
-                            duration={duration}
-                            setDuration={setDuration}
-                        />
+					</div>
 
-                    </div>
+				</div>
+			}
 
-                </div>
-            }
+			{isEditOpen &&
+				<CovePresetsModal
+					setIsEditOpen={setIsEditOpen}
+					channelState={state}
+					setState={setState}
+					presets={presets}
+					setPresets={setPresets}
+					setMasterValue={setMasterValue}
+				/>
+			}
+		</>
 
-            {isEditOpen &&
-                <CovePresetsModal
-                    setIsEditOpen={setIsEditOpen}
-                    channelState={state}
-                    setState={setState}
-                    presets={presets}
-                    setPresets={setPresets}
-                    setMasterValue={setMasterValue}
-                />
-            }
-        </>
-
-    );
+	);
 };
 
 
