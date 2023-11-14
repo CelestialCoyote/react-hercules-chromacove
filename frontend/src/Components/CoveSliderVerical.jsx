7c004bddaeaf8d6ac0ebde91b559b7e017d3c1c1
Paul Stearns <celestialcoyote@gmail.com>
2023-11-13 18:36:07 -0800

    cannot figure out state changing to undefined

--- frontend/src/Components/CoveSliderVerical.jsx
+++ frontend/src/Components/CoveSliderVerical.jsx
@@ -3,127 +3,265 @@ import Slider from '@mui/material/Slider';
 import { baseAPI } from '../userConfig/baseAPI';
 
 
-const CoveSliderVertical = ({ coveColor, color, setLevel, level }) => {
+const CoveSliderVertical = ({ coveColor, channelState, setState, masterValue, duration }) => {
+	console.log('channelStat: ', channelState);
 
-	const [isOff, setIsOff] = useState(true);
-	const [temp, setTemp] = useState(0);
+    const [temp, setTemp] = useState(0.000);
 
-	const debounce = (func, wait, immediate) => {
-		let timeout;
+    const updateChannelState = (channelId, value) => {
+        setState(prevState => (
+            { ...prevState, [channelId]: { ...prevState[channelId], value: value } }
+        ));
+    };
 
-		return function () {
-			let context = this;
-			let args = arguments;
+    const debounce = (func, wait, immediate) => {
+        let timeout;
 
-			let later = function () {
-				timeout = null;
-				if (!immediate) func.apply(context, args);
-			};
+        return function () {
+            let context = this;
+            let args = arguments;
 
-			let callNow = immediate && !timeout;
+            let later = function () {
+                timeout = null;
+                if (!immediate) func.apply(context, args);
+            };
 
-			clearTimeout(timeout);
-			timeout = setTimeout(later, wait);
-			if (callNow) func.apply(context, args);
-		};
-	};
+            let callNow = immediate && !timeout;
 
-	const handleSliderChange = (event) => {
-		setLevel(event.target.value);
-		if (event.target.value > 0) setIsOff(false);
-		if (event.target.value === 0) {
-			setTemp(0);
-			setIsOff(true);
-		}
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) func.apply(context, args);
+        };
+    };
 
-		let data = {
-			"channel": coveColor,
-			"level": event.target.value,
-			"duration": 0.01
-		};
+    const sendColorData = (currentValue, isSlider) => {
+        const data = [];
+        let channelDuration = 0.01;
 
-		try {
-			baseAPI.post('colorChangeSlider', data)
-				.then((res) => {
-					console.log(res.data);
-				});
-		} catch (error) {
-			console.log('Update color channel failed.', error);
-		};
-	};
+        if (isSlider)
+            channelDuration = 0.01;
+        else
+            channelDuration = duration;
 
-	const handleToggleButton = () => {
-		if (level > 0.000) {
-			setTemp(level);
-			setLevel(0.000);
-			setIsOff(true);
-		} else {
-			if (temp === 0.000) {
-				setTemp(1.000);
-				setLevel(1.000)
-			} else {
-				setLevel(temp);
-			}
-			setIsOff(false);
-		};
-	};
+        data.push({
+            "id": channelState.id,
+            "name": channelState.name,
+            "value": currentValue,
+            "slider": channelState.slider
+        });
+        data.push({ "name": "master", "value": masterValue, "duration": channelDuration });
 
-	const coveButtonBase = "border-red-500 border-2 text-xl rounded-xl w-24";
-	const coveButton = "bg-black text-red-500 hover:border-red-300 hover:text-red-300";
-	const coveButtonOn = "bg-red-500 text-black hover:bg-red-300 hover:border-red-300";
+        try {
+            baseAPI.post('colorChange', data)
+                .then((res) => {
+                    console.log(res.data);
+                });
+        } catch (error) {
+            console.log('Update color channel failed.', error);
+        };
+    };
 
-	return (
+    const handleToggleButton = () => {
+        let newLevel;
+        if (channelState.value > 0.000) {
+            setTemp(channelState.value);
+            updateChannelState(channelState.id, 0.000);
+            newLevel = 0.000;
+        } else {
+            if (temp === 0.000) {
+                setTemp(1.000);
+                updateChannelState(channelState.id, 1.000);
+                newLevel = 1.000;
+            } else {
+                updateChannelState(channelState.id, temp);
+                newLevel = temp;
+            }
+        }
 
-		<div className="flex flex-col items-center">
+        sendColorData(newLevel);
+    };
 
-			<button
-                className={isOff ? 
-					`${coveButtonBase} ${coveButton}`
-					:
-					`${coveButtonBase} ${coveButtonOn}`
-				}
-				onClick={handleToggleButton}
+    const handleColorChange = (event) => {
+        updateChannelState(channelState.id, event.target.value);
+
+        if (event.target.value === 0.000) setTemp(0);
+
+        sendColorData(event.target.value, true);
+    };
+
+    return (
+
+        <div className="cove-slider-group">
+
+            <button
+                className={channelState.value > 0 ? "toggle-button-on" : "toggle-button"}
+                onClick={handleToggleButton}
             >
-                {coveColor} {isOff ? "On" : "Off"}
+                {channelState.name.toUpperCase()}<br />{channelState.value > 0 ? "On" : "Off"}
             </button>
 
-			<label className="text-xl text-red-500 mt-4 mb-4">{
-				coveColor.toUpperCase()}
-			</label>
+            <label className="cove-text">{
+                channelState.name.toUpperCase()}
+                <br />
+                {channelState.value.toFixed(3)}
+            </label>
 
-			<label className="text-xl text-red-500 mb-6">
-				{level.toFixed(3)}
-			</label>
+            <Slider
+                sx={{
+                    color: {coveColor},
+                    height: "100%",
+                    '& .MuiSlider-thumb': {
+                        borderRadius: '0.25rem',
+                        height: "1.5rem",
+                        width: "3.0rem"
+                    },
+                    '& .MuiSlider-rail': {
+                        borderRadius: '0.25rem',
+                        width: "1.0rem"
+                    },
+                    '& .MuiSlider-track': {
+                        width: "0.5rem"
+                    }
+                }}
+                orientation="vertical"
+                min={0.000}
+                max={1.000}
+                step={0.001}
+                value={channelState.value}
+                onChange={debounce(handleColorChange)}
+            />
 
-			<Slider
-				sx={{
-					color: { color },
-					height: "100%",
-					'& .MuiSlider-thumb': {
-						borderRadius: '0.25rem',
-						height: "1.5rem",
-						width: "3.0rem"
-					},
-					'& .MuiSlider-rail': {
-						borderRadius: '0.25rem',
-						width: "1.0rem"
-					},
-					'& .MuiSlider-track': {
-						width: "0.5rem"
-					}
-				}}
-				orientation="vertical"
-				min={0.000}
-				max={1.000}
-				step={0.001}
-				value={level}
-				onChange={debounce(handleSliderChange)}
-			/>
+        </div>
 
-		</div>
-
-	);
-};
+    );
+}
 
 
 export default CoveSliderVertical;
+
+
+
+// const CoveSliderVertical = ({ coveColor, color, setLevel, level }) => {
+
+// 	const [isOff, setIsOff] = useState(true);
+// 	const [temp, setTemp] = useState(0);
+
+// 	const debounce = (func, wait, immediate) => {
+// 		let timeout;
+
+// 		return function () {
+// 			let context = this;
+// 			let args = arguments;
+
+// 			let later = function () {
+// 				timeout = null;
+// 				if (!immediate) func.apply(context, args);
+// 			};
+
+// 			let callNow = immediate && !timeout;
+
+// 			clearTimeout(timeout);
+// 			timeout = setTimeout(later, wait);
+// 			if (callNow) func.apply(context, args);
+// 		};
+// 	};
+
+// 	const handleSliderChange = (event) => {
+// 		setLevel(event.target.value);
+// 		if (event.target.value > 0) setIsOff(false);
+// 		if (event.target.value === 0) {
+// 			setTemp(0);
+// 			setIsOff(true);
+// 		}
+
+// 		let data = {
+// 			"channel": coveColor,
+// 			"level": event.target.value,
+// 			"duration": 0.01
+// 		};
+
+// 		try {
+// 			baseAPI.post('colorChangeSlider', data)
+// 				.then((res) => {
+// 					console.log(res.data);
+// 				});
+// 		} catch (error) {
+// 			console.log('Update color channel failed.', error);
+// 		};
+// 	};
+
+// 	const handleToggleButton = () => {
+// 		if (level > 0.000) {
+// 			setTemp(level);
+// 			setLevel(0.000);
+// 			setIsOff(true);
+// 		} else {
+// 			if (temp === 0.000) {
+// 				setTemp(1.000);
+// 				setLevel(1.000)
+// 			} else {
+// 				setLevel(temp);
+// 			}
+// 			setIsOff(false);
+// 		};
+// 	};
+
+// 	const coveButtonBase = "border-red-500 border-2 text-xl rounded-xl w-24";
+// 	const coveButton = "bg-black text-red-500 hover:border-red-300 hover:text-red-300";
+// 	const coveButtonOn = "bg-red-500 text-black hover:bg-red-300 hover:border-red-300";
+
+// 	return (
+
+// 		<div className="flex flex-col items-center">
+
+// 			<button
+//                 className={isOff ? 
+// 					`${coveButtonBase} ${coveButton}`
+// 					:
+// 					`${coveButtonBase} ${coveButtonOn}`
+// 				}
+// 				onClick={handleToggleButton}
+//             >
+//                 {coveColor} {isOff ? "On" : "Off"}
+//             </button>
+
+// 			<label className="text-xl text-red-500 mt-4 mb-4">{
+// 				coveColor.toUpperCase()}
+// 			</label>
+
+// 			<label className="text-xl text-red-500 mb-6">
+// 				{level.toFixed(3)}
+// 			</label>
+
+// 			<Slider
+// 				sx={{
+// 					color: { color },
+// 					height: "100%",
+// 					'& .MuiSlider-thumb': {
+// 						borderRadius: '0.25rem',
+// 						height: "1.5rem",
+// 						width: "3.0rem"
+// 					},
+// 					'& .MuiSlider-rail': {
+// 						borderRadius: '0.25rem',
+// 						width: "1.0rem"
+// 					},
+// 					'& .MuiSlider-track': {
+// 						width: "0.5rem"
+// 					}
+// 				}}
+// 				orientation="vertical"
+// 				min={0.000}
+// 				max={1.000}
+// 				step={0.001}
+// 				value={level}
+// 				onChange={debounce(handleSliderChange)}
+// 			/>
+
+// 		</div>
+
+// 	);
+// };
+
+
+// export default CoveSliderVertical;
--- frontend/src/pages/RGBCove/RGBCove.jsx
+++ frontend/src/pages/RGBCove/RGBCove.jsx
@@ -1,41 +1,39 @@
+//import { useEffect, useState } from 'react';
 import { useState } from 'react';
 import CoveSliderVertical from '../../components/CoveSliderVerical';
 import CoveMasterSlider from '../../components/CoveMasterSlider';
 //import { baseAPI } from '../../userConfig/baseAPI';
 
 
+const rgbChannels = [
+	{
+		"id": 1,
+		"name": "red",
+		"color": "#bb0000",
+		"value": 0.000,
+		"slider": true
+	},
+	{
+		"id": 2,
+		"name": "grn",
+		"color": "#00bb00",
+		"value": 0.000,
+		"slider": true
+	},
+	{
+		"id": 3,
+		"name": "blu",
+		"color": "#0077bb",
+		"value": 0.000,
+		"slider": true
+	},
+];
+
 export default function RGBCove() {
-	const [red, setRed] = useState(0.000);
-	const [grn, setGrn] = useState(0.000);
-	const [blu, setBlu] = useState(0.000);
+	const [state, setState] = useState(rgbChannels);
 	const [masterValue, setMasterValue] = useState(1.000);
 	const [duration, setDuration] = useState(3);
 
-
-	const [state, setState] = useState({});
-	const [channelData, setChannelData] = useState([]);
-	const [channelState, setChannelState] = useState({});
-	const [rgbChannels, setRGBChannels] = useState(
-		[
-			{
-				"id": 1,
-				"name": "red",
-				"value": 0.000,
-				"slider": true
-			},
-			{
-				"id": 2,
-				"name": "grn",
-				"value": 0.000,
-				"slider": true
-			},
-			{
-				"id": 3,
-				"name": "blu",
-				"value": 0.000,
-				"slider": true
-			},
-		]);
 
 	return (
 		<div className="flex flex-1 flex-col w-1/2 m-6">
@@ -43,13 +41,18 @@ export default function RGBCove() {
 			<div className="flex flex-1 flex-col">
 
 				<div className="flex h-full justify-between">
-					<CoveSliderVertical
-						coveColor="Red"
-						color="#bb0000"
-						setLevel={setRed}
-						level={red}
-					/>
-					<CoveSliderVertical
+					{rgbChannels.map(channel =>
+						<CoveSliderVertical
+							key={channel.id}
+							coveColor={channel.name}
+							color={channel.color}
+							channelState={state[channel.id]}
+							setState={setState}
+							masterValue={masterValue}
+							duration={duration}
+						/>
+					)}
+					{/* <CoveSliderVertical
 						coveColor="Grn"
 						color="#00bb00"
 						setLevel={setGrn}
@@ -60,7 +63,7 @@ export default function RGBCove() {
 						color="#0077bb"
 						setLevel={setBlu}
 						level={blu}
-					/>
+					/> */}
 				</div>
 
 				<div className="flex flex-col mt-4">
