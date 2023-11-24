932ba282b15e9d1e77e4e2a2653163e569d3ee10
Paul Stearns <celestialcoyote@gmail.com>
2023-11-23 18:34:14 -0800

    minor css change to fade select

--- frontend/src/Components/FadeRateSelect.jsx
+++ frontend/src/Components/FadeRateSelect.jsx
@@ -2,23 +2,19 @@ export default function FadeRateSelect({ duration, setDuration }) {
 	const fadeRates = [2, 3, 5, 10, 30, 60];
 
 	return (
-
 		<div className="border-red-500 text-red-500 border-t-4 mt-10">
 
-			<h3
-				className="text-center mt-6"
-			>
+			<h3 className="text-center mt-6">
 				Fade Rate Selection
 			</h3>
 
 			<div className="flex flex-col">
-
 				<div className="flex justify-center mb-6">
 					<h3 className="mr-2">
 						Current Fade Rate (Duration):
 					</h3>
 
-					<h3 className="">
+					<h3 className="font-bold text-red-300">
 						{duration} seconds
 					</h3>
 				</div>
@@ -45,11 +41,7 @@ export default function FadeRateSelect({ duration, setDuration }) {
 						</button>
 					))}
 				</div>
-
 			</div>
-
 		</div>
-
-	)
-
+	);
 };
