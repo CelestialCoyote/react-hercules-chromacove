d7708c0a535292808cbc569b70bb672d3821742c
Paul Stearns <celestialcoyote@gmail.com>
2023-11-24 18:31:12 -0800

    working on new nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
-import Nav from './components/Nav';
+// import Nav from './components/Nav';
+import Navigation from './components/Navigation';
 import Footer from './components/Footer';
-import Home from './pages/Home/home';
+// import Home from './pages/Home/home';
 import RGBCove from './pages/RGBCove/RGBCove';
 import StarMachineLamps from './pages/StarMachineLamps/StarMachineLamps';
 import StarMachineMotions from './pages/StarMachineMotions/StarMachineMotions';
@@ -11,10 +12,11 @@ const App = () => {
 
 	return (
 		<div className="flex flex-col h-screen w-full">
-			<Nav />
+			{/* <Nav /> */}
+			<Navigation /> 
 			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/rgbcove" element={<RGBCove />} />
+				<Route path="/" element={<RGBCove />} />
+				{/* <Route path="/rgbcove" element={<RGBCove />} /> */}
 				<Route path="/starmachinelamps" element={<StarMachineLamps />} />
 				<Route path="/starmachinemotions" element={<StarMachineMotions />} />
 			</Routes>
