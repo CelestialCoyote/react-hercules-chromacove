91b97074668144ea8dd2caf04bb145496d22b9bc
Paul Stearns <celestialcoyote@gmail.com>
2023-12-07 19:47:45 -0800

    searchable select test

--- frontend/src/pages/StarMachineLamps/StarMachineLamps.jsx
+++ frontend/src/pages/StarMachineLamps/StarMachineLamps.jsx
@@ -1,8 +1,23 @@
+import { useState } from "react";
+import SearchableSelect from ".././../components/SearchableSelect";
+import { animals } from "../../userConfig/animals";
+import "../../components/SearchableSelect.css";
+
+
 export default function StarMachineLamps() {
+	const [value, setValue] = useState("Select option...");
 
 	return (
-        <div className="flex justify-center items-center h-screen bg-black-900 text-red-500">
-            <h1 className="text-3xl">Star Machine Lamps</h1>
-        </div>
-    );
+		<div className="flex flex-col bg-black-900 text-red-500">
+			<h1 className="text-3xl text-center w-full">Star Machine Lamps</h1>
+
+			<SearchableSelect
+				options={animals}
+				label="name"
+				id="id"
+				selectedVal={value}
+				handleChange={(val) => setValue(val)}
+			/>
+		</div>
+	);
 };
